944f3211b3feb173c63f5fcbcd34c729874eb239
Brad Carlson <b2carlso@yahoo.com>
2018-05-09 23:21:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 class Driver {
   constructor(name, startDate) {
     this.name = name;
-    this.startDate = startDate;
-  }
-
-  startDate() {
-    
+    this.startDate = new Date(startDate);
   }
 }
